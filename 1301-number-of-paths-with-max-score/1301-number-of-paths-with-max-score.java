ebdf1cf233358856f177e1a453a11f4106ffa2c9
Mohak Gupta <campionite0@gmail.com>
2026-07-05 21:59:46 +0530

    Time: 12 ms (77.65%), Space: 47.2 MB (18.82%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
@@ -6,31 +6,24 @@ class Solution {
         long[][] dp = new long[n + 1][n + 1];
         long[][] cnt = new long[n + 1][n + 1];
         
-        // Convert board to char grid for easy access
         char[][] grid = new char[n][n];
         for (int i = 0; i < n; i++) {
             grid[i] = board.get(i).toCharArray();
         }
         
-        // Initialize with -1 (unreachable) except start point
         for (long[] row : dp) Arrays.fill(row, -1);
         
         dp[n - 1][n - 1] = 0;
         cnt[n - 1][n - 1] = 1;
         
-        // Directions: up, left, upper-left diagonal
-        int[][] dirs = {{-1, 0}, {0, -1}, {-1, -1}};
-        
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if (i == n - 1 && j == n - 1) continue; // start cell already set
-                if (grid[i][j] == 'X') continue; // obstacle
+                if (i == n - 1 && j == n - 1) continue;
+                if (grid[i][j] == 'X') continue;
                 
                 long best = -1;
                 long ways = 0;
                 
-                // Look at cells that lead INTO (i, j): 
-                // i.e., (i+1, j), (i, j+1), (i+1, j+1)
                 int[][] from = {{i + 1, j}, {i, j + 1}, {i + 1, j + 1}};
                 
                 for (int[] f : from) {
