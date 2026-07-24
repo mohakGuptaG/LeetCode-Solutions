ca1b3226a6d018ee553d6df10e2f3847b3c31a2e
Mohak Gupta <campionite0@gmail.com>
2026-07-24 22:53:42 +0530

    Time: 1321 ms (27.27%), Space: 47.1 MB (49.09%) - LeetHub

---
+++ 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.java
@@ -0,0 +1,30 @@
+import java.util.BitSet;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) {
+            return 1;
+        }
+
+        Set<Integer> pairXor = new HashSet<>();
+        BitSet tripletXor = new BitSet();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                pairXor.add(nums[i] ^ nums[j]);
+            }
+        }
+
+        for (int x : pairXor) {
+            for (int num : nums) {
+                tripletXor.set(x ^ num);
+            }
+        }
+
+        return tripletXor.cardinality();
+    }
+}
