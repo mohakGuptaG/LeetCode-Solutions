ea2ff285b4f50b9557638990e2d384bbaa1b581e
Mohak Gupta <campionite0@gmail.com>
2026-08-06 12:02:37 +0530

    Time: 0 ms (100%), Space: 43.7 MB (10.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,19 +1,42 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int n = nums.length;
-
-        for(int i=n; i>1; i--){
-            int last = i;
-
-            for(int j=0; j<last-1; j++){
-                if(nums[j]>nums[j+1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                }
-            }
+    public static void msort(int[] arr, int low, int high){
+        if(low<high){
+            int pivotIndex = func(arr, low, high);
+            msort(arr, low, pivotIndex-1);
+            msort(arr, pivotIndex+1, high);
         }
 
-        
+        return;
+    }
+
+    public static int func(int[] arr, int low, int high){
+        int pivot = arr[low];
+        int i = low, j = high;
+        while(i<j){
+            while(arr[i]<=pivot && i<high){
+                i++;
+            }
+
+            while(arr[j]>pivot && j>low){
+                j--;
+            }
+
+            if(i<j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            } 
+        }
+                int temp = arr[j];
+                arr[j] = arr[low];
+                arr[low] = temp;
+        return j;
+    }
+
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        msort(nums, low, high);
+        return;
     }
 }
