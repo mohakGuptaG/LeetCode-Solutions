fbefece9655551e697359c4e45238729cf414ea0
Mohak Gupta <campionite0@gmail.com>
2026-08-14 23:23:19 +0530

    Time: 7 ms (63.49%), Space: 43.6 MB (99.19%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String reverseWords(String s) {
+        
+        boolean flag = false;
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder ans = new StringBuilder();
+
+        int k = 0, j = s.length()-1;
+
+        while(k<s.length()){
+            if(s.charAt(k)!=' ') break;
+            k++;
+        }
+
+        while(j>=0){
+            if(s.charAt(j)!=' '){
+                break;
+            }
+            j--;
+        }
+
+        for(int i=j; i>=k; i--){
+
+            if(s.charAt(i)==' ' && flag){
+                continue;
+            }
+
+            else if(s.charAt(i)!=' '){
+                sb.append(s.charAt(i));
+                flag = false;
+            }
+
+            else{
+                ans.append(sb.reverse());
+                ans.append(" ");
+                sb.delete(0, sb.length());
+                flag = true;
+            }
+        }
+
+        ans.append(sb.reverse());
+
+        return ans.toString();
+    }
+}
