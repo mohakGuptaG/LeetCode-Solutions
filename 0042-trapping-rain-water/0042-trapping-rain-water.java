41c921f9f41bca32c3eabfaf500d01c60572e423
Mohak Gupta <campionite0@gmail.com>
2026-09-10 21:15:00 +0530

    Time: 0 ms (100%), Space: 47.6 MB (66.69%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,43 +1,31 @@
 class Solution {
     public int trap(int[] height) {
-        Stack<Integer> maxPrefix = new Stack<>();
-        Stack<Integer> maxSuffix = new Stack<>();
-
-        int[] prefix = new int[height.length];
-        int[] suffix = new int[height.length];
-
-        maxPrefix.add(height[0]);
-
-        for(int i=0; i<height.length; i++){
-            if(height[i]>maxPrefix.peek()){
-                maxPrefix.add(height[i]);
-            }
-
-            prefix[i] = maxPrefix.peek();
-        }
-
-        maxSuffix.add(height[height.length-1]);
-
-        for(int i=height.length-1; i>=0; i--){
-            if(height[i]>maxSuffix.peek()){
-                maxSuffix.add(height[i]);
-            }
-
-            suffix[i] = maxSuffix.peek();
-        }
-
-        int[] min = new int[height.length];
-
-        for(int i=0; i<height.length; i++){
-            min[i] = Math.min(prefix[i], suffix[i]);
-        }
-
+        int left = 0;
+        int right = height.length-1;
+        int leftMax = 0;
+        int rightMax = 0;
         int water = 0;
 
-        for(int i=0; i<height.length; i++){
-            water += min[i] - height[i];
+        while(left<right){
+            if(height[left]<=height[right]){
+                if(height[left]>=leftMax){
+                    leftMax = height[left];
+                }
+                else{
+                    water += leftMax - height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right]>=rightMax){
+                    rightMax = height[right];
+                }
+                else{
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
         }
-
         return water;
-    } 
+    }
 }
