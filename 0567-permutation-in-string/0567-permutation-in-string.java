b456f3de4fdceb5a2c4d973909bf31e2ecfa4272
Mohak Gupta <campionite0@gmail.com>
2026-09-11 16:12:16 +0530

    Time: 6 ms (87.46%), Space: 43.7 MB (96.63%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,6 +2,8 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int[] freqA = new int[26];
 
+        if(s1.length()>s2.length()) return false;
+
         for(int i=0; i<s1.length(); i++){
             freqA[s1.charAt(i)-'a']++;
         }
@@ -9,16 +11,25 @@ class Solution {
         int left = 0;
         int right = s1.length()-1;
 
-        while(right<s2.length()){
-            int[] freqB = new int[26];
+        int[] freqB = new int[26];
             for(int i=left; i<=right; i++){
                 freqB[s2.charAt(i)-'a']++;
             }
+            
             if(Arrays.equals(freqA, freqB)){
                 return true;
             }
+
+
+        while(right<s2.length()-1){
+            freqB[s2.charAt(left)-'a']--;
             left++;
             right++;
+            freqB[s2.charAt(right)-'a']++;
+            
+            if(Arrays.equals(freqA, freqB)){
+                return true;
+            }
         }
         return false;
     }
